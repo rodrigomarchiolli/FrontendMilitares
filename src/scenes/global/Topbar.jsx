27f03e42aa2159ece615001c7e5b8e941580d6f8
CodeWracker <dev.rodrigofs@gmail.com>
2022-12-16 00:28:05 -0300

    arrumei p build

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,79 +1,80 @@
-import { Box, IconButton, useTheme} from "@mui/material";
+import { Box, IconButton, useTheme } from "@mui/material";
 import { useContext } from "react";
 import { ColorModeContext, tokens } from "../../theme";
 import InputBase from "@mui/material/InputBase";
-import { ipcRenderer } from 'electron';
 
-import  LightModeOutlinedIcon  from "@mui/icons-material/LightModeOutlined";
-import  DarkModeOutlinedIcon  from "@mui/icons-material/DarkModeOutlined";
-import  NotificationsOutlinedIcon  from "@mui/icons-material/NotificationsOutlined";
-import  SettingsOutlinedIcon  from "@mui/icons-material/SettingsOutlined";
-import  PersonOutlinedIcon  from "@mui/icons-material/PersonOutlined";
-import  SearchIcon  from "@mui/icons-material/Search";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchIcon from "@mui/icons-material/Search";
 import CloseIcon from '@mui/icons-material/Close';
 import MinimizeIcon from '@mui/icons-material/Minimize';
 import FilterNoneIcon from '@mui/icons-material/FilterNone';
 //import { borderRadius } from "@mui/system";
 
+const ipcRenderer = window.require('electron').ipcRenderer;
+
 function closeApp() {
     ipcRenderer.send('close-app');
-  }
+}
 
 function minimizeApp() {
-ipcRenderer.send('minimize-app');
+    ipcRenderer.send('minimize-app');
 }
 
 function expandApp() {
     ipcRenderer.send('expand-app');
-  }
+}
 
 
-const Topbar = () =>{
+const Topbar = () => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
     const colorMode = useContext(ColorModeContext);
 
     return (
         <Box display="flex" justifyContent="space-between" p={2}>
-            {/* SEARCH BAR */ }
-            <Box 
-                display= "flex" 
-                backgroundColor={colors.primary[400]} 
-                borderRadius = "3px">
+            {/* SEARCH BAR */}
+            <Box
+                display="flex"
+                backgroundColor={colors.primary[400]}
+                borderRadius="3px">
 
-                <InputBase sx ={{ml: 2, flex: 1}} placeholder ="Search"/>
-                
-                <IconButton type = "button" sx={{ p: 1 }}>
-                    <SearchIcon/>
+                <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+
+                <IconButton type="button" sx={{ p: 1 }}>
+                    <SearchIcon />
                 </IconButton>
-                
+
             </Box>
 
             {/* ICONS */}
-            <Box display= "flex">
+            <Box display="flex">
                 <IconButton onClick={colorMode.toggleColorMode}>
-                    {theme.palette.mode ==='dark'? (
-                        <DarkModeOutlinedIcon/>
-                    ) :(
-                        <LightModeOutlinedIcon/>
+                    {theme.palette.mode === 'dark' ? (
+                        <DarkModeOutlinedIcon />
+                    ) : (
+                        <LightModeOutlinedIcon />
                     )}
-                    
+
                 </IconButton>
-                    
-                <IconButton>
-                    <MinimizeIcon onClick={() => minimizeApp()}/>
-                    
-                </IconButton>
-                    
 
                 <IconButton>
-                    <FilterNoneIcon fontSize="small" onClick={() => expandApp()}/>
-                    
+                    <MinimizeIcon onClick={() => minimizeApp()} />
+
                 </IconButton>
-                    
+
+
                 <IconButton>
-                    <CloseIcon onClick={() => closeApp()}/>
-                    
+                    <FilterNoneIcon fontSize="small" onClick={() => expandApp()} />
+
+                </IconButton>
+
+                <IconButton>
+                    <CloseIcon onClick={() => closeApp()} />
+
                 </IconButton>
             </Box>
         </Box>
